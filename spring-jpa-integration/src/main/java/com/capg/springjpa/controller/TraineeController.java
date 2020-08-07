a271faaa2377ff8f3abd2281f624cae169124a61
praveen bandi <praveen bandi@LAPTOP-J82473UA>
2020-08-07 17:25:21 +0530

    spring jpa integration

--- spring-jpa-integration/src/main/java/com/capg/springjpa/controller/TraineeController.java
+++ spring-jpa-integration/src/main/java/com/capg/springjpa/controller/TraineeController.java
@@ -3,18 +3,22 @@ package com.capg.springjpa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capg.springjpa.model.Trainee;
 import com.capg.springjpa.service.ITraineeService;
 
 @RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class TraineeController {
 
 	@Autowired
@@ -36,7 +40,7 @@ public class TraineeController {
 	{
 		return service.getAllTrainees();
 	}
-	@DeleteMapping("/trainees/id/{id}")
+	@DeleteMapping("/deleteTrainee/id/{id}")
 	public void deleteTrainee(@PathVariable int id)
 	{
 		service.deleteTrainee(id);
